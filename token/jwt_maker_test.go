dd8c404825d1510b59a1ce7fadb93012a5142c75
raqazhet <qazhetrazaq@gmail.com>
2023-06-20 19:18:17 +0600

    hello

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,28 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"bank/util"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJwtMaker(t *testing.T) {
+	maker, err := NewJwtMaker(util.RandomString(32))
+	require.NoError(t, err)
+	username := util.RandomOwner()
+	duration := time.Minute
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
