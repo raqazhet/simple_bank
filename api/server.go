bdc1071f910e0257083db931bbda3ba6fa4a1ad2
raqazhet <qazhetrazaq@gmail.com>
2023-06-20 20:27:05 +0600

    g

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	"bank/jsonlog"
 	"bank/storage"
+	"bank/token"
+	"bank/util"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -10,13 +12,22 @@ import (
 )
 
 type Server struct {
-	store  storage.Store
-	router *gin.Engine
-	l      *jsonlog.Logger
+	store      storage.Store
+	router     *gin.Engine
+	l          *jsonlog.Logger
+	tokenMaker token.Maker
 }
 
 func NewServer(store storage.Store, l *jsonlog.Logger) *Server {
-	server := &Server{store: store, l: l}
+	tokenMaker, err := token.NewJwtMaker(util.RandomString(32))
+	if err != nil {
+		return nil
+	}
+	server := &Server{
+		store:      store,
+		l:          l,
+		tokenMaker: tokenMaker,
+	}
 	router := gin.Default()
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
